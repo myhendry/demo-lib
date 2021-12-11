e4dfefd0e6de699d6e51a0ca05928054afb14d98
Hendry Lim <myhendry@gmail.com>
2021-12-11 22:05:30 +0800

    added shadow

--- pages/demo/three-d/index.tsx
+++ pages/demo/three-d/index.tsx
@@ -2,22 +2,42 @@ import { NextPage } from "next";
 import { useRef } from "react";
 import { Canvas, useFrame } from "react-three-fiber";
 import Link from "next/link";
-import * as three from "three";
+import * as THREE from "three";
 import { Stats, OrbitControls } from "@react-three/drei";
+import { Color } from "three";
 
 interface Props {}
 
 const Box = (props: any) => {
-  const ref = useRef<three.Mesh>();
+  const ref = useRef<THREE.Mesh>();
   useFrame((state) => {
     ref.current!.rotation.x += 0.01;
     ref.current!.rotation.y += 0.01;
   });
 
   return (
-    <mesh ref={ref} {...props}>
+    <mesh ref={ref} {...props} castShadow receiveShadow>
       <boxBufferGeometry />
-      <meshBasicMaterial color={"blue"} />
+      <meshPhysicalMaterial color={"blue"} />
+    </mesh>
+  );
+};
+
+const Floor = (props: any) => {
+  return (
+    <mesh {...props} receiveShadow>
+      <boxBufferGeometry args={[20, 1, 10]} />
+      <meshPhysicalMaterial color={"white"} />
+    </mesh>
+  );
+};
+
+const Bulb = (props: any) => {
+  return (
+    <mesh {...props}>
+      <pointLight castShadow />
+      <sphereBufferGeometry args={[0.2, 20, 20]} />
+      <meshPhongMaterial emissive={new Color(0xff0000)} />
     </mesh>
   );
 };
@@ -33,10 +53,18 @@ const ThreeD: NextPage<Props> = (props) => {
           <a className="text-white p-2">Go Back</a>
         </Link>
       </div>
-      <Canvas style={{ background: "black" }} camera={{ position: [3, 3, 3] }}>
+      <Canvas
+        shadows
+        style={{ background: "black" }}
+        camera={{ position: [3, 3, 3] }}
+      >
+        <ambientLight intensity={0.2} />
+
+        <Bulb position={[0, 3, 0]} />
         <Stats />
         <OrbitControls />
         <Box position={[-1, 1, 2]} />
+        <Floor postition={[0, -0.5, 0]} />
         <axesHelper args={[3]} />
       </Canvas>
     </div>
