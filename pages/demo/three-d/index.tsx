8a2e7c07be9ada3faa49dd6957224e1ba2814dce
Hendry Lim <myhendry@gmail.com>
2021-12-11 20:09:30 +0800

    testing three-react-fiber

--- pages/demo/three-d/index.tsx
+++ pages/demo/three-d/index.tsx
@@ -1,14 +1,27 @@
 import { NextPage } from "next";
+import { Canvas } from "react-three-fiber";
+import Link from "next/link";
 
 import { Layout } from "../../../components/common";
 
 interface Props {}
 
 const ThreeD: NextPage<Props> = (props) => {
+  // https://threejs.org/manual/#en/fundamentals
+  // https://github.com/pmndrs/react-three-fiber
+
   return (
-    <Layout>
-      <p>Using Three Js</p>
-    </Layout>
+    <div style={{ width: "100vw", height: "100vw" }}>
+      <Link href={"/"}>
+        <a>Go Back</a>
+      </Link>
+      <Canvas style={{ background: "black" }}>
+        <mesh>
+          <boxBufferGeometry />
+          <meshBasicMaterial color={"blue"} />
+        </mesh>
+      </Canvas>
+    </div>
   );
 };
 
